3f900e7f6f95f75a44ae5e2473089dc80a9afcb6
luke <luketao@icloud.com>
2024-06-03 09:36:58 -0400

    modified dashboard to search page place holder

--- app/src/main/java/com/example/stalk/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/stalk/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.stalk.ui.dashboard
+package com.example.stalk.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Search placeholder page"
     }
     val text: LiveData<String> = _text
 }
