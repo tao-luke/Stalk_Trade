6b324ccdc1adf09e8df1a63d3a1b626abc460b5a
wais-hundekar <wais.hundekar@gmail.com>
2024-06-11 21:37:44 -0400

    saved placeholder changes

--- app/src/main/java/com/example/stalk/ui/saved/SavedFragment.kt
+++ app/src/main/java/com/example/stalk/ui/saved/SavedFragment.kt
@@ -1,4 +1,4 @@
-package com.example.stalk.ui.notifications
+package com.example.stalk.ui.saved
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.stalk.databinding.FragmentNotificationsBinding
+import com.example.stalk.databinding.FragmentSavedBinding
 
-class NotificationsFragment : Fragment() {
+class SavedFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentSavedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class NotificationsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+                ViewModelProvider(this).get(SavedViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSavedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textSaved
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
