59ea096bfdf53272a8aea002bd15286fa39fab23
Ethan Maeda <emaeda@uwaterloo.ca>
2024-07-04 15:28:03 -0400

    try scheduled workflow, update script
    update workflow for test
    
    test 3:45
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    Create db.log
    
    Update daily.yml
    
    Update requirements.txt
    
    log total new trades added, run workflow every 12 hours and output log
    
    remove logger, print instead
    
    remove tee db.log
    
    try to get output to appear in annotations
    
    setup annotations
    
    Update daily.yml
    
    Update daily.yml
    
    use notice, not debug
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    Update daily.yml
    
    revert back to prints
    
    update script

--- python/stalk-backfill.py
+++ python/stalk-backfill.py
@@ -1,12 +1,21 @@
+import json
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
 import hashlib
+import logging
 from datetime import datetime, timedelta
+import os
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("stalk-db-firebase-adminsdk-g64um-cd9cc7e2a2.json")
+current_dir = os.path.dirname(os.path.realpath(__file__))
+json_file = 'stalk-db-firebase-adminsdk-g64um-cd9cc7e2a2.json'
+json_path = os.path.join(current_dir, json_file)
+
+# Initialize Firebase Admin SDK with the service account key
+cred = credentials.Certificate(json_path)
 firebase_admin.initialize_app(cred)
+
+annotations = []
 
 class Trade:
     def __init__(self, firstName, lastName, transactionDate, owner, assetDescription, type, amount, link, dateRecieved, ticker):
@@ -85,28 +94,61 @@ def name_exists(collection_ref, first_name, last_name):
 
 # Function to upload trade data to Firestore
 def upload_trade_data_to_firestore(data, collection):
+    new = []
+    one_year_ago = datetime.now() - timedelta(days=365)
     db = firestore.client()
     collection_ref = db.collection(collection)
-    # Assuming data is a dictionary or list of dictionaries
+    
     if isinstance(data, dict):
         # Calculate hash of content
         content_hash = calculate_content_hash(str(data))
         # Check if document with the same hash already exists
         if not document_exists(collection_ref, content_hash):
-            # Add hash to data
-            data["hash"] = content_hash
-            # Add data to Firestore
-            collection_ref.add(data)
+            # Parse the transaction date
+            date_str = data.get('transactionDate')
+            if date_str:
+                try:
+                    date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+                    if date_obj >= one_year_ago:
+                        # Add hash to data
+                        data["hash"] = content_hash
+                        # Add data to Firestore
+                        collection_ref.add(data)
+                        new.append(data)
+                        print("New trade added: ", data)
+                    else:
+                        print(f"Skipping old trade: {data}")
+                except ValueError as e:
+                    print(f"::error::Error parsing date: {e}")
+            else:
+                print(f"::error::No transaction date found in data: {data}")
+
     elif isinstance(data, list):
         for entry in data:
             # Calculate hash of content
             content_hash = calculate_content_hash(str(entry))
             # Check if document with the same hash already exists
             if not document_exists(collection_ref, content_hash):
-                # Add hash to entry
-                entry["hash"] = content_hash
-                # Add entry to Firestore
-                collection_ref.add(entry)
+                # Parse the transaction date
+                date_str = entry.get('transactionDate')
+                if date_str:
+                    try:
+                        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+                        if date_obj >= one_year_ago:
+                            # Add hash to entry
+                            entry["hash"] = content_hash
+                            # Add entry to Firestore
+                            collection_ref.add(entry)
+                            new.append(entry)
+                            print("New trade added: ", entry)
+                        else:
+                            print(f"Skipping old trade: {entry}")
+                    except ValueError as e:
+                        print(f"::error::Error parsing date: {e}")
+                else:
+                    print(f"::error::No transaction date found in entry: {entry}")
+
+    return new
 
 # Function to upload unique names to Firestore
 def upload_names_to_firestore(data):
@@ -124,42 +166,7 @@ def upload_names_to_firestore(data):
             }
             names_collection_ref.add(name_data)
 
-            print(f"Adding name: {first_name} {last_name}")
-
-def delete_old_entries(collection):
-    one_year_ago = datetime.now() - timedelta(days=365)
-    db = firestore.client()
-    collection_ref = db.collection(collection)
-    docs = collection_ref.stream()
-    
-    for doc in docs:
-        doc_data = doc.to_dict()
-        date_str = doc_data.get('transactionDate')
-        
-        if date_str:
-            try:
-                date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-                if date_obj < one_year_ago:
-                    print(f"Deleting document ID: {doc.id} with date: {date_str}")
-                    collection_ref.document(doc.id).delete()
-            except ValueError as e:
-                print(f"Error parsing date for document ID: {doc.id} - {e}")
-
-def trading_backfill(collection):
-    for i in range(0, 31):
-        data = fetch_data_senate_trading(i)
-        parsed_data = parse_senate_trading(data)
-        upload_trade_data_to_firestore(parsed_data, collection)
-        upload_names_to_firestore(parsed_data)
-        print("Page ", i, " complete")
-
-def disclosure_backfill(collection):
-    for i in range(0, 31):
-        data = fetch_data_senate_disclosure(i)
-        parsed_data = parse_senate_disclosure(data)
-        upload_trade_data_to_firestore(parsed_data, collection)
-        upload_names_to_firestore(parsed_data)
-        print("Page ", i, " complete")
+            print(f"::debug::Adding name: {first_name} {last_name}")
 
 def remove_unused_names():
     db = firestore.client()
@@ -176,29 +183,24 @@ def remove_unused_names():
         trades_query = all_trades_collection_ref.where("firstName", "==", first_name).where("lastName", "==", last_name).limit(1).get()
         
         if len(trades_query) == 0:
-            print(f"Deleting unused name: {first_name} {last_name}")
+            print("Deleting unused name: {first_name} {last_name}")
             names_collection_ref.document(name_doc.id).delete()
 
 def update(collection):
     data = fetch_data_senate_disclosure(0)
     parsed_data = parse_senate_disclosure(data)
-    upload_trade_data_to_firestore(parsed_data, collection)
+    new_trades1 = upload_trade_data_to_firestore(parsed_data, collection)
     upload_names_to_firestore(parsed_data)
-
-    print("Disclosure update complete")
 
     data = fetch_data_senate_trading(0)
     parsed_data = parse_senate_trading(data)
-    upload_trade_data_to_firestore(parsed_data, collection)
+    new_trades2 = upload_trade_data_to_firestore(parsed_data, collection)
+
     upload_names_to_firestore(parsed_data)
-
-    print("Trading update complete")
-
-    delete_old_entries(collection)
 
     remove_unused_names()
 
-    print("Update complete")
+    print("Total of " + str(len(new_trades1 + new_trades2)) + " new trades added")
 
 def main():
     update("all_trades")
