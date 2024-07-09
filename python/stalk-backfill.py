63a785a4016b175a064560b73ff32a7902e8dfa3
Ethan Maeda <emaeda@uwaterloo.ca>
2024-07-09 14:45:10 -0400

    update script

--- python/stalk-backfill.py
+++ python/stalk-backfill.py
@@ -94,32 +94,61 @@ def name_exists(collection_ref, first_name, last_name):
 
 # Function to upload trade data to Firestore
 def upload_trade_data_to_firestore(data, collection):
-    count = 0
+    new = []
+    one_year_ago = datetime.now() - timedelta(days=365)
     db = firestore.client()
     collection_ref = db.collection(collection)
-    # Assuming data is a dictionary or list of dictionaries
+    
     if isinstance(data, dict):
         # Calculate hash of content
         content_hash = calculate_content_hash(str(data))
         # Check if document with the same hash already exists
         if not document_exists(collection_ref, content_hash):
-            # Add hash to data
-            data["hash"] = content_hash
-            # Add data to Firestore
-            collection_ref.add(data)
-            count += 1
+            # Parse the transaction date
+            date_str = data.get('transactionDate')
+            if date_str:
+                try:
+                    date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+                    if date_obj >= one_year_ago:
+                        # Add hash to data
+                        data["hash"] = content_hash
+                        # Add data to Firestore
+                        collection_ref.add(data)
+                        new.append(data)
+                        print("New trade added: ", data)
+                    else:
+                        print(f"Skipping old trade: {data}")
+                except ValueError as e:
+                    print(f"::error::Error parsing date: {e}")
+            else:
+                print(f"::error::No transaction date found in data: {data}")
+
     elif isinstance(data, list):
         for entry in data:
             # Calculate hash of content
             content_hash = calculate_content_hash(str(entry))
             # Check if document with the same hash already exists
             if not document_exists(collection_ref, content_hash):
-                # Add hash to entry
-                entry["hash"] = content_hash
-                # Add entry to Firestore
-                collection_ref.add(entry)
-                count += 1
-    return count
+                # Parse the transaction date
+                date_str = entry.get('transactionDate')
+                if date_str:
+                    try:
+                        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+                        if date_obj >= one_year_ago:
+                            # Add hash to entry
+                            entry["hash"] = content_hash
+                            # Add entry to Firestore
+                            collection_ref.add(entry)
+                            new.append(entry)
+                            print("New trade added: ", entry)
+                        else:
+                            print(f"Skipping old trade: {entry}")
+                    except ValueError as e:
+                        print(f"::error::Error parsing date: {e}")
+                else:
+                    print(f"::error::No transaction date found in entry: {entry}")
+
+    return new
 
 # Function to upload unique names to Firestore
 def upload_names_to_firestore(data):
@@ -139,25 +168,6 @@ def upload_names_to_firestore(data):
             names_collection_ref.add(name_data)
 
             print(f"::debug::Adding name: {first_name} {last_name}")
-
-def delete_old_entries(collection):
-    one_year_ago = datetime.now() - timedelta(days=365)
-    db = firestore.client()
-    collection_ref = db.collection(collection)
-    docs = collection_ref.stream()
-    
-    for doc in docs:
-        doc_data = doc.to_dict()
-        date_str = doc_data.get('transactionDate')
-        
-        if date_str:
-            try:
-                date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-                if date_obj < one_year_ago:
-                    # print(f"Deleting document ID: {doc.id} with date: {date_str}")
-                    collection_ref.document(doc.id).delete()
-            except ValueError as e:
-                print(f"::error::Error parsing date for document ID: {doc.id} - {e}")
 
 def remove_unused_names():
     db = firestore.client()
@@ -186,24 +196,15 @@ def update(collection):
     data = fetch_data_senate_trading(0)
     parsed_data = parse_senate_trading(data)
     new_trades2 = upload_trade_data_to_firestore(parsed_data, collection)
-    upload_names_to_firestore(parsed_data)
 
-    delete_old_entries(collection)
+    upload_names_to_firestore(parsed_data)
 
     remove_unused_names()
 
-    print("Total of " + str(new_trades1 + new_trades2) + " new trades added")
+    print("Total of " + str(len(new_trades1 + new_trades2)) + " new trades added")
 
 def main():
     update("all_trades")
-
-    # Combine all annotations into one structure
-    output_data = {
-        "annotations": annotations
-    }
-
-    # Print JSON formatted data
-    print(json.dumps(output_data))
 
 if __name__ == "__main__":
     main()
