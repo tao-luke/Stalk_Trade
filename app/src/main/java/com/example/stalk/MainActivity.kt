55fba4394c88124293129a244cbee7ed9cba1f60
luke <luketao@icloud.com>
2024-07-11 22:43:00 -0400

    implement notification permission pop-up

--- app/src/main/java/com/example/stalk/MainActivity.kt
+++ app/src/main/java/com/example/stalk/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.stalk
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -26,6 +30,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestNotificationPermission()
+        } else {
+            setupNavigation()
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            val requestPermissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted, proceed with setup
+                    setupNavigation()
+                } else {
+                    // Permission is denied, show a message and exit
+                    Toast.makeText(this, "Notification permission is required for this app, please enable it in setting.", Toast.LENGTH_LONG).show()
+                    finish() // Exit the app
+                }
+            }
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            // Permission already granted
+            setupNavigation()
+        }
+    }
+
+    private fun setupNavigation() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
